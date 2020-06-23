7e5347996062bb3691b40502735fcf7bab29e425
Abdulrahman Alsaedi <codingelab@gmail.com>
2020-06-24 01:20:46 +0300

    feature: implements either strategey partially

--- either.go
+++ either.go
@@ -1,1 +1,20 @@
 package index
+
+type immutableEitherStrategy struct {
+	first Index
+	last  Index
+}
+
+func Either(first, last Index) Index {
+	return immutableEitherStrategy{
+		first: first,
+		last:  last,
+	}
+}
+func (index immutableEitherStrategy) Get(value string) (int, error) {
+	i, err := index.first.Get(value)
+	if err != nil {
+		i, err = index.last.Get(value)
+	}
+	return i, err
+}
--- either_test.go
+++ either_test.go
@@ -1,1 +1,43 @@
 package index_test
+
+import (
+	"testing"
+
+	"github.com/begopher/index"
+)
+
+func TestGetInEitherStrategyWithStrictAndLength(t *testing.T) {
+	testCases := strictIndexTestTable()
+	testCases = append(testCases, lengthTestTable()...)
+	for _, test := range testCases {
+		strict, err := index.Strict(test.indexOf, test.occur, test.exclusive, test.ltr)
+		if err != nil {
+			t.Errorf("should not return error [%v]", err.Error())
+		}
+		length := index.Length()
+		either := index.Either(strict, length)
+		got, _ := either.Get(test.value)
+		expected := test.expected
+		if got != expected {
+			t.Errorf("Expected value [%v], got [%v]", expected, got)
+		}
+	}
+}
+func TestGetInEitherStrategyWithStrictAndZero(t *testing.T) {
+	testCases := strictIndexTestTable()
+	testCases = append(testCases, zeroTestTable()...)
+	for _, test := range testCases {
+		strict, err := index.Strict(test.indexOf, test.occur, test.exclusive, test.ltr)
+		if err != nil {
+			t.Errorf("should not return error [%v]", err.Error())
+		}
+		zero := index.Zero()
+		either := index.Either(strict, zero)
+		got, _ := either.Get(test.value)
+		expected := test.expected
+		if got != expected {
+			t.Errorf("Expected value [%v], got [%v]", expected, got)
+		}
+	}
+
+}
